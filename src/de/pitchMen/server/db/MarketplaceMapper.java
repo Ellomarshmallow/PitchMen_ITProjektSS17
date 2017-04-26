69458ee12f1165adaa5a23238d73caf3acff7e2c
HeikeDrews <hd018@hdm-stuttgart.de>
2017-04-26 17:10:03 +0200

    MarketplaceMapper hinzugefügt

---
+++ src/de/pitchMen/server/db/MarketplaceMapper.java
@@ -0,0 +1,96 @@
+package de.pitchMen.server.db;
+
+/**
+ * muss manuel importiert werden
+ * @author Heike
+ *
+ */
+
+import java.sql.*;
+import java.util.Vector;
+
+public class MarketplaceMapper {
+
+	
+	    /**
+	     * Default constructor
+	     */
+	    public MarketplaceMapper() {
+	    }
+
+	    /**
+	     * Fügt ein Marketplace-Objekt der Datenbank hinzu.
+	     * 
+	     * @param marketplace 
+	     * @return
+	     */
+	    public Marketplace insert(Marketplace marketplace) {
+	        // TODO implement here
+	        return null;
+	    }
+
+	    /**
+	     * Aktuallisiert ein Marketplace-Objekt in der Datenbank.
+	     * 
+	     * @param marketplace 
+	     * @return
+	     */
+	    public Marketplace update(Marketplace marketplace) {
+	        // TODO implement here
+	        return null;
+	    }
+
+	    /**
+	     * Löscht ein Marketplace-Objekt aus der Datenbank.
+	     * 
+	     * @param marketplace 
+	     * @return
+	     */
+	    public void delete(Marketplace marketplace) {
+	        // TODO implement here
+	        return null;
+	    }
+
+	    /**
+	     * Findet ein Marketplace-Objekt anhand der übergebenen Id in der Datenbank.
+	     * 
+	     * @param id 
+	     * @return
+	     */
+	    public Marketplace findById(int id) {
+	        // TODO implement here
+	        return null;
+	    }
+
+	    /**
+	     * Findet alle Marketplace-Objekte in der Datenbank.
+	     * 
+	     * @return
+	     */
+	    public ArrayList<Marketplace> findAll() {
+	        // TODO implement here
+	        return null;
+	    }
+
+	    /**
+	     * Findet ein Marketplace-Objekt anhand des übergebenen Titels in der Datenbank.
+	     * 
+	     * @param title 
+	     * @return
+	     */
+	    public ArrayList<Marketplace> findByTitle(String title) {
+	        // TODO implement here
+	        return null;
+	    }
+
+	    /**
+	     * @return
+	     */
+	    public static MarketplaceMapper marketplaceMapper() {
+	        // TODO implement here
+	        return null;
+	    }
+
+	
+	
+}
