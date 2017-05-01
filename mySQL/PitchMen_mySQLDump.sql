70535d0cc4b5e77947d6c0c2ab0b5a69696849eb
LeenenLars <ll038@hdm-stuttgart.de>
2017-05-01 16:01:51 +0200

    Schema-Name geändert

--- mySQL/PitchMen_mySQLDump.sql
+++ mySQL/PitchMen_mySQLDump.sql
@@ -5,25 +5,25 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
 -- -----------------------------------------------------
--- Schema PitchMen
+-- Schema pitchmen_itprojekt_schema
 -- -----------------------------------------------------
 -- This database contains all tables to realize our Business-Logic. 
-DROP SCHEMA IF EXISTS `PitchMen` ;
+DROP SCHEMA IF EXISTS `pitchmen_itprojekt_schema` ;
 
 -- -----------------------------------------------------
--- Schema PitchMen
+-- Schema pitchmen_itprojekt_schema
 --
 -- This database contains all tables to realize our Business-Logic. 
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `PitchMen` DEFAULT CHARACTER SET latin1 COLLATE latin1_german1_ci ;
-USE `PitchMen` ;
+CREATE SCHEMA IF NOT EXISTS `pitchmen_itprojekt_schema` DEFAULT CHARACTER SET utf8 ;
+USE `pitchmen_itprojekt_schema` ;
 
 -- -----------------------------------------------------
--- Table `PitchMen`.`company`
+-- Table `pitchmen_itprojekt_schema`.`company`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `PitchMen`.`company` ;
+DROP TABLE IF EXISTS `pitchmen_itprojekt_schema`.`company` ;
 
-CREATE TABLE IF NOT EXISTS `PitchMen`.`company` (
+CREATE TABLE IF NOT EXISTS `pitchmen_itprojekt_schema`.`company` (
   `id` INT NOT NULL,
   `name` VARCHAR(45) NULL,
   `description` VARCHAR(45) NULL,
@@ -32,11 +32,11 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `PitchMen`.`team`
+-- Table `pitchmen_itprojekt_schema`.`team`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `PitchMen`.`team` ;
+DROP TABLE IF EXISTS `pitchmen_itprojekt_schema`.`team` ;
 
-CREATE TABLE IF NOT EXISTS `PitchMen`.`team` (
+CREATE TABLE IF NOT EXISTS `pitchmen_itprojekt_schema`.`team` (
   `id` INT NOT NULL,
   `name` VARCHAR(45) NULL,
   `description` VARCHAR(45) NULL,
@@ -45,11 +45,11 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `PitchMen`.`person`
+-- Table `pitchmen_itprojekt_schema`.`person`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `PitchMen`.`person` ;
+DROP TABLE IF EXISTS `pitchmen_itprojekt_schema`.`person` ;
 
-CREATE TABLE IF NOT EXISTS `PitchMen`.`person` (
+CREATE TABLE IF NOT EXISTS `pitchmen_itprojekt_schema`.`person` (
   `id` INT NOT NULL,
   `name` VARCHAR(45) NULL,
   `description` VARCHAR(45) NULL,
@@ -59,11 +59,11 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `PitchMen`.`partnerProfile`
+-- Table `pitchmen_itprojekt_schema`.`partnerProfile`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `PitchMen`.`partnerProfile` ;
+DROP TABLE IF EXISTS `pitchmen_itprojekt_schema`.`partnerProfile` ;
 
-CREATE TABLE IF NOT EXISTS `PitchMen`.`partnerProfile` (
+CREATE TABLE IF NOT EXISTS `pitchmen_itprojekt_schema`.`partnerProfile` (
   `id` INT NOT NULL,
   `dateCreated` DATE NULL,
   `dateChanged` DATE NULL,
@@ -76,28 +76,28 @@ CREATE TABLE IF NOT EXISTS `PitchMen`.`partnerProfile` (
   INDEX `fk_partnerProfile_person1_idx` (`person_id` ASC),
   CONSTRAINT `fk_partnerProfile_company`
     FOREIGN KEY (`company_id`)
-    REFERENCES `PitchMen`.`company` (`id`)
+    REFERENCES `pitchmen_itprojekt_schema`.`company` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_partnerProfile_team1`
     FOREIGN KEY (`team_id`)
-    REFERENCES `PitchMen`.`team` (`id`)
+    REFERENCES `pitchmen_itprojekt_schema`.`team` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_partnerProfile_person1`
     FOREIGN KEY (`person_id`)
-    REFERENCES `PitchMen`.`person` (`id`)
+    REFERENCES `pitchmen_itprojekt_schema`.`person` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `PitchMen`.`marketplace`
+-- Table `pitchmen_itprojekt_schema`.`marketplace`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `PitchMen`.`marketplace` ;
+DROP TABLE IF EXISTS `pitchmen_itprojekt_schema`.`marketplace` ;
 
-CREATE TABLE IF NOT EXISTS `PitchMen`.`marketplace` (
+CREATE TABLE IF NOT EXISTS `pitchmen_itprojekt_schema`.`marketplace` (
   `id` INT NOT NULL,
   `description` VARCHAR(45) NULL,
   `title` VARCHAR(45) NULL,
@@ -110,28 +110,28 @@ CREATE TABLE IF NOT EXISTS `PitchMen`.`marketplace` (
   INDEX `fk_marketplace_company1_idx` (`company_id` ASC),
   CONSTRAINT `fk_marketplace_person1`
     FOREIGN KEY (`person_id`)
-    REFERENCES `PitchMen`.`person` (`id`)
+    REFERENCES `pitchmen_itprojekt_schema`.`person` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_marketplace_team1`
     FOREIGN KEY (`team_id`)
-    REFERENCES `PitchMen`.`team` (`id`)
+    REFERENCES `pitchmen_itprojekt_schema`.`team` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_marketplace_company1`
     FOREIGN KEY (`company_id`)
-    REFERENCES `PitchMen`.`company` (`id`)
+    REFERENCES `pitchmen_itprojekt_schema`.`company` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `PitchMen`.`project`
+-- Table `pitchmen_itprojekt_schema`.`project`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `PitchMen`.`project` ;
+DROP TABLE IF EXISTS `pitchmen_itprojekt_schema`.`project` ;
 
-CREATE TABLE IF NOT EXISTS `PitchMen`.`project` (
+CREATE TABLE IF NOT EXISTS `pitchmen_itprojekt_schema`.`project` (
   `id` INT NOT NULL,
   `title` VARCHAR(45) NULL,
   `description` VARCHAR(45) NULL,
@@ -144,22 +144,22 @@ CREATE TABLE IF NOT EXISTS `PitchMen`.`project` (
   INDEX `fk_project_person1_idx` (`person_id` ASC),
   CONSTRAINT `fk_project_marketplace1`
     FOREIGN KEY (`marketplace_id`)
-    REFERENCES `PitchMen`.`marketplace` (`id`)
+    REFERENCES `pitchmen_itprojekt_schema`.`marketplace` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_project_person1`
     FOREIGN KEY (`person_id`)
-    REFERENCES `PitchMen`.`person` (`id`)
+    REFERENCES `pitchmen_itprojekt_schema`.`person` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
 
 
 -- -----------------------------------------------------
--- Table `PitchMen`.`jobPosting`
+-- Table `pitchmen_itprojekt_schema`.`jobPosting`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `PitchMen`.`jobPosting` ;
+DROP TABLE IF EXISTS `pitchmen_itprojekt_schema`.`jobPosting` ;
 
-CREATE TABLE IF NOT EXISTS `PitchMen`.`jobPosting` (
+CREATE TABLE IF NOT EXISTS `pitchmen_itprojekt_schema`.`jobPosting` (
   `id` INT NOT NULL,
   `title` VARCHAR(45) NULL,
   `text` VARCHAR(255) NULL,
@@ -171,23 +171,23 @@ CREATE TABLE IF NOT EXISTS `PitchMen`.`jobPosting` (
   INDEX `fk_jobPosting_project1_idx` (`project_id` ASC),
   CONSTRAINT `fk_jobPosting_partnerProfile1`
     FOREIGN KEY (`partnerProfile_id`)
-    REFERENCES `PitchMen`.`partnerProfile` (`id`)
+    REFERENCES `pitchmen_itprojekt_schema`.`partnerProfile` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_jobPosting_project1`
     FOREIGN KEY (`project_id`)
-    REFERENCES `PitchMen`.`project` (`id`)
+    REFERENCES `pitchmen_itprojekt_schema`.`project` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `PitchMen`.`trait`
+-- Table `pitchmen_itprojekt_schema`.`trait`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `PitchMen`.`trait` ;
+DROP TABLE IF EXISTS `pitchmen_itprojekt_schema`.`trait` ;
 
-CREATE TABLE IF NOT EXISTS `PitchMen`.`trait` (
+CREATE TABLE IF NOT EXISTS `pitchmen_itprojekt_schema`.`trait` (
   `id` INT NOT NULL,
   `name` VARCHAR(45) NULL,
   `value` VARCHAR(45) NULL,
@@ -196,18 +196,18 @@ CREATE TABLE IF NOT EXISTS `PitchMen`.`trait` (
   INDEX `fk_trait_partnerProfile1_idx` (`partnerProfile_id` ASC),
   CONSTRAINT `fk_trait_partnerProfile1`
     FOREIGN KEY (`partnerProfile_id`)
-    REFERENCES `PitchMen`.`partnerProfile` (`id`)
+    REFERENCES `pitchmen_itprojekt_schema`.`partnerProfile` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `PitchMen`.`application`
+-- Table `pitchmen_itprojekt_schema`.`application`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `PitchMen`.`application` ;
+DROP TABLE IF EXISTS `pitchmen_itprojekt_schema`.`application` ;
 
-CREATE TABLE IF NOT EXISTS `PitchMen`.`application` (
+CREATE TABLE IF NOT EXISTS `pitchmen_itprojekt_schema`.`application` (
   `id` INT NOT NULL,
   `text` VARCHAR(255) NULL,
   `dateCreated` DATE NULL,
@@ -216,18 +216,18 @@ CREATE TABLE IF NOT EXISTS `PitchMen`.`application` (
   INDEX `fk_application_jobPosting1_idx` (`jobPosting_id` ASC),
   CONSTRAINT `fk_application_jobPosting1`
     FOREIGN KEY (`jobPosting_id`)
-    REFERENCES `PitchMen`.`jobPosting` (`id`)
+    REFERENCES `pitchmen_itprojekt_schema`.`jobPosting` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `PitchMen`.`rating`
+-- Table `pitchmen_itprojekt_schema`.`rating`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `PitchMen`.`rating` ;
+DROP TABLE IF EXISTS `pitchmen_itprojekt_schema`.`rating` ;
 
-CREATE TABLE IF NOT EXISTS `PitchMen`.`rating` (
+CREATE TABLE IF NOT EXISTS `pitchmen_itprojekt_schema`.`rating` (
   `id` INT NOT NULL,
   `statement` VARCHAR(45) NULL,
   `score` FLOAT NULL,
@@ -236,18 +236,18 @@ CREATE TABLE IF NOT EXISTS `PitchMen`.`rating` (
   INDEX `fk_rating_application1_idx` (`application_id` ASC),
   CONSTRAINT `fk_rating_application1`
     FOREIGN KEY (`application_id`)
-    REFERENCES `PitchMen`.`application` (`id`)
+    REFERENCES `pitchmen_itprojekt_schema`.`application` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `PitchMen`.`participation`
+-- Table `pitchmen_itprojekt_schema`.`participation`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `PitchMen`.`participation` ;
+DROP TABLE IF EXISTS `pitchmen_itprojekt_schema`.`participation` ;
 
-CREATE TABLE IF NOT EXISTS `PitchMen`.`participation` (
+CREATE TABLE IF NOT EXISTS `pitchmen_itprojekt_schema`.`participation` (
   `id` INT NOT NULL,
   `workload` FLOAT NULL,
   `dateClosed` DATE NULL,
@@ -257,80 +257,80 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `PitchMen`.`person_has_participation`
+-- Table `pitchmen_itprojekt_schema`.`person_has_participation`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `PitchMen`.`person_has_participation` ;
+DROP TABLE IF EXISTS `pitchmen_itprojekt_schema`.`person_has_participation` ;
 
-CREATE TABLE IF NOT EXISTS `PitchMen`.`person_has_participation` (
+CREATE TABLE IF NOT EXISTS `pitchmen_itprojekt_schema`.`person_has_participation` (
   `person_id` INT NOT NULL,
   `participation_id` INT NOT NULL,
   INDEX `fk_person_has_participation_participation1_idx` (`participation_id` ASC),
   INDEX `fk_person_has_participation_person1_idx` (`person_id` ASC),
   CONSTRAINT `fk_person_has_participation_person1`
     FOREIGN KEY (`person_id`)
-    REFERENCES `PitchMen`.`person` (`id`)
+    REFERENCES `pitchmen_itprojekt_schema`.`person` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_person_has_participation_participation1`
     FOREIGN KEY (`participation_id`)
-    REFERENCES `PitchMen`.`participation` (`id`)
+    REFERENCES `pitchmen_itprojekt_schema`.`participation` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `PitchMen`.`company_has_participation`
+-- Table `pitchmen_itprojekt_schema`.`company_has_participation`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `PitchMen`.`company_has_participation` ;
+DROP TABLE IF EXISTS `pitchmen_itprojekt_schema`.`company_has_participation` ;
 
-CREATE TABLE IF NOT EXISTS `PitchMen`.`company_has_participation` (
+CREATE TABLE IF NOT EXISTS `pitchmen_itprojekt_schema`.`company_has_participation` (
   `company_id` INT NOT NULL,
   `participation_id` INT NOT NULL,
   INDEX `fk_company_has_participation_participation1_idx` (`participation_id` ASC),
   INDEX `fk_company_has_participation_company1_idx` (`company_id` ASC),
   CONSTRAINT `fk_company_has_participation_company1`
     FOREIGN KEY (`company_id`)
-    REFERENCES `PitchMen`.`company` (`id`)
+    REFERENCES `pitchmen_itprojekt_schema`.`company` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_company_has_participation_participation1`
     FOREIGN KEY (`participation_id`)
-    REFERENCES `PitchMen`.`participation` (`id`)
+    REFERENCES `pitchmen_itprojekt_schema`.`participation` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `PitchMen`.`participation_has_team`
+-- Table `pitchmen_itprojekt_schema`.`participation_has_team`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `PitchMen`.`participation_has_team` ;
+DROP TABLE IF EXISTS `pitchmen_itprojekt_schema`.`participation_has_team` ;
 
-CREATE TABLE IF NOT EXISTS `PitchMen`.`participation_has_team` (
+CREATE TABLE IF NOT EXISTS `pitchmen_itprojekt_schema`.`participation_has_team` (
   `participation_id` INT NOT NULL,
   `team_id` INT NOT NULL,
   INDEX `fk_participation_has_team_team1_idx` (`team_id` ASC),
   INDEX `fk_participation_has_team_participation1_idx` (`participation_id` ASC),
   CONSTRAINT `fk_participation_has_team_participation1`
     FOREIGN KEY (`participation_id`)
-    REFERENCES `PitchMen`.`participation` (`id`)
+    REFERENCES `pitchmen_itprojekt_schema`.`participation` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_participation_has_team_team1`
     FOREIGN KEY (`team_id`)
-    REFERENCES `PitchMen`.`team` (`id`)
+    REFERENCES `pitchmen_itprojekt_schema`.`team` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `PitchMen`.`participation_has_project`
+-- Table `pitchmen_itprojekt_schema`.`participation_has_project`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `PitchMen`.`participation_has_project` ;
+DROP TABLE IF EXISTS `pitchmen_itprojekt_schema`.`participation_has_project` ;
 
-CREATE TABLE IF NOT EXISTS `PitchMen`.`participation_has_project` (
+CREATE TABLE IF NOT EXISTS `pitchmen_itprojekt_schema`.`participation_has_project` (
   `participation_id` INT NOT NULL,
   `project_id` INT NOT NULL,
   PRIMARY KEY (`participation_id`, `project_id`),
@@ -338,12 +338,12 @@ CREATE TABLE IF NOT EXISTS `PitchMen`.`participation_has_project` (
   INDEX `fk_participation_has_project_participation1_idx` (`participation_id` ASC),
   CONSTRAINT `fk_participation_has_project_participation1`
     FOREIGN KEY (`participation_id`)
-    REFERENCES `PitchMen`.`participation` (`id`)
+    REFERENCES `pitchmen_itprojekt_schema`.`participation` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_participation_has_project_project1`
     FOREIGN KEY (`project_id`)
-    REFERENCES `PitchMen`.`project` (`id`)
+    REFERENCES `pitchmen_itprojekt_schema`.`project` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
