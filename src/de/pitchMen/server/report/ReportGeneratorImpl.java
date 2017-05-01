b3a130a23203667bb448eb7387366363e5673409
LeenenLars <ll038@hdm-stuttgart.de>
2017-05-01 22:46:49 +0200

    Package ..server.report hinzugefügt. ReportGeneratorImpl dem o.g. Package hinzugefügt.

---
+++ src/de/pitchMen/server/report/ReportGeneratorImpl.java
@@ -0,0 +1,21 @@
+package de.pitchMen.server.report;
+
+import java.util.Date;
+import java.util.Vector;
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+
+/**
+ * Implemetierungsklasse des Interface ReportGenerator.  Sie enthält die Applikationslogik, stellt die Zusammenhänge konstistent dar und ist zuständig für einen geordneten Ablauf.
+ * 
+ * @author
+ */
+public class ReportGeneratorImpl extends RemoteServiceServlet {
+
+    /**
+     * Default constructor
+     */
+    public ReportGeneratorImpl() {
+    }
+
+
+}
