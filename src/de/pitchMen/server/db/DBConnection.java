04fcfc65160be42d9d52ff4d52d8207b54335b44
Sharkoon <Sharkoon@192.168.0.65>
2017-05-02 19:12:25 +0200

    DBConnection um die locale URL erweitert und die Authentifizierung der URL's angepasst.

--- src/de/pitchMen/server/db/DBConnection.java
+++ src/de/pitchMen/server/db/DBConnection.java
@@ -1,18 +1,14 @@
 package de.pitchMen.server.db;
 
-/**
- * @author Heike
- */
-
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
+
 import com.google.appengine.api.utils.SystemProperty;
 
 /**
  * Klasse zum Aufbau zur Verbindung der Datenbank.
  * 
- * @author
+ * @author Heike, Lars
  */
 
 public class DBConnection {
@@ -22,14 +18,17 @@ public class DBConnection {
 	 */
 
 	private static Connection con = null;
-	static final String jdbc_driver = "com.mysql.jdbc.Driver";
+
 	/**
 	 * Mit dieser Url wird die Datenbank angesprochen.
 	 */
-	//static final String user = "root";
-	//static final String pass = "";
-	static final String googleUrl = "jdbc:google:mysql://173.194.105.139:3306/pitchmen_itprojekt_schema?user=root&password=";
-
+	static final String user = "root";
+	static final String pass = "winter2017";
+	static final String dbName = "pitchmen_itprojekt_schema";
+	static final String googleUrl = "jdbc:google:mysql://173.194.105.139:3306/" + dbName + "?user=" + user
+			+ "&password=" + pass + "";
+	private static String localUrl = "jdbc:mysql://127.0.0.1:3306/" + dbName + "?user=" + user + "&password=" + pass
+			+ "";
 
 	/**
 	 * Stellt die Verbindung zur Datenbank her.
@@ -39,23 +38,36 @@ public class DBConnection {
 	 */
 
 	public static Connection connection() throws ClassNotFoundException {
-
 		/*
 		 * Herstellung einer DB Verbindung, wenn bisher keine Verbindung besteht
 		 */
 		if (con == null) {
 			String url = null;
 			try {
-				Class.forName(jdbc_driver);
-				url = googleUrl;
-
+				if (SystemProperty.environment.value() == SystemProperty.Environment.Value.Production) {
+					Class.forName("com.mysql.jdbc.GoogleDriver");
+					url = googleUrl;
+				} else {
+					/**
+					 * Wenn die googleUrl nicht erreichbar ist, wird die LocaleUrl aufgerufen.
+					 * Alternativlösung während des Entwickelns.
+					 */
+					Class.forName("com.mysql.jdbc.Driver");
+					url = localUrl;
+				}
+				/**
+				 * Die Verbindung zur Datenbank wird in der Variablen 
+				 * con mit den dazugehörigen Informationen gespeichert
+				 */
 				con = DriverManager.getConnection(url);
 			} catch (Exception e) {
 				con = null;
 				e.printStackTrace();
 			}
 		}
-
+		/**
+		 * Verbindung wird in der Variable con zurückgegeben
+		 */
 		return con;
 	}
 
