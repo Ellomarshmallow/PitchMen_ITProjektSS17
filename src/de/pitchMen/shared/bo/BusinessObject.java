e00aced41c7c89da1b79cba48bb7262f64749efb
EleonoraRenz <eleonora.renz96@web.de>
2017-05-02 18:27:58 +0200

    Klassen BusinessObject und Application überarbeitet

--- src/de/pitchMen/shared/bo/BusinessObject.java
+++ src/de/pitchMen/shared/bo/BusinessObject.java
@@ -3,52 +3,63 @@ package de.pitchMen.shared.bo;
 import java.io.Serializable;
 
 /**
- * Alle Klassen des packages bo erben von dieser Superklasse.
+ * Alle Klassen des Paket bo erben von dieser Superklasse.
+ * 
+ * @author EleonoraRenz
  */
 public abstract class BusinessObject implements Serializable {
 
-    /**
-     * 
-     */
-    private int id;
+	private int id = 0;
 
-    /**
-     * 
-     */
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    /**
-     * @return
-     */
-    public int getId() {
-        // TODO implement here
-        return 0;
-    }
+	/**
+	 * @return id
+	 */
+	public int getId() {
+		return this.id;
+	}
 
-    /**
-     * @param id 
-     * @return
-     */
-    public void setId(int id) {
-        // TODO implement here
-        return null;
-    }
+	/**
+	 * @param id
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
 
-    /**
-     * @return
-     */
-    public String toString() {
-        // TODO implement here
-        return "";
-    }
+	/**
+	 * Gibt den Klassennamen + jeweilige
+	 * 
+	 * @return id wieder.
+	 */
+	public String toString() {
+		return this.getClass().getName() + " #" + this.id;
+	}
 
-    /**
-     * @param object 
-     * @return
-     */
-    public boolean equals(Object object) {
-        // TODO implement here
-        return false;
-    }
+	/**
+	 * Vergleicht zwei Objekte auf ihre inhaltliche Gleichheit. Momentan nur auf
+	 * eine ID.
+	 * 
+	 * @param object
+	 */
+	public boolean equals(Object o) {
+		if (o != null && o instanceof BusinessObject) {
+			BusinessObject bo = (BusinessObject) o;
+			try {
+				if (bo.getId() == this.id)
+					return true;
+			} catch (IllegalArgumentException e) {
+				return false;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * @return id
+	 */
+	public int hashCode() {
+		return this.id;
+	}
 
 }
