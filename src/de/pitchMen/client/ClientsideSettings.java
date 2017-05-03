ec3653c289e94cb606e678190e2fe24bdf721d69
SimonKienzler <simon.kienzler@gmx.de>
2017-05-03 16:12:23 +0200

    ClientsideSettings-Klasse angelegt

---
+++ src/de/pitchMen/client/ClientsideSettings.java
@@ -0,0 +1,108 @@
+package de.pitchMen.client;
+
+import java.util.logging.Logger;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import de.pitchMen.shared.*;
+
+/**
+ * Diese Klasse enthält Dienste und Eigenschaften, die für Klassen
+ * des client-Packages relevant sind.
+ * 
+ * @author Simon
+ * 
+ */
+
+public class ClientsideSettings extends CommonSettings {
+	
+	  /**
+	   * Remote Service Proxy. Hiermit wird eine Verbindung mit dem Server-seitgen Dienst
+	   * <code>PitchMenAdmin</code> hergestellt.
+	   */
+	  private static PitchMenAdminAsync pitchMenAdmin = null;
+
+	  /**
+	   * Remote Service Proxy. Hiermit wird eine Verbindung mit dem Server-seitgen Dienst
+	   * <code>ReportGenerator</code> hergestellt.
+	   */
+	  
+	  private static ReportGeneratorAsync reportGenerator = null;
+
+	  /**
+	   * Name des Client-Loggers.
+	   */
+	  private static final String LOGGER_NAME = "PitchMen Client";
+	  
+	  /**
+	   * Instanz des Client-seitigen Loggers.
+	   */
+	  private static final Logger log = Logger.getLogger(LOGGER_NAME);
+
+	  /**
+	   * <p>
+	   * Auslesen des Client-Loggers.
+	   * </p>
+	   * 
+	   * @return die Logger-Instanz für die Server-Seite
+	   */
+	  public static Logger getLogger() {
+	    return log;
+	  }
+
+	  /**
+	   * <p>
+	   * Anlegen und Auslesen der applikationsweit eindeutigen PitchMenAdmin. Diese
+	   * Methode erstellt die PitchMenAdmin, wenn sie noch nicht existiert. Ansonsten 
+	   * wird das bereits angelegte Objekt übergeben.
+	   * </p>
+	   * 
+	   * @return eindeutige Instanz des Typs <code>PitchMenAdminAsync</code>
+	   * @author Simon
+	   */
+	  public static PitchMenAdminAsync getPitchMenAdmin() {
+	    if (pitchMenAdmin == null) {
+	      // falls noch nicht geschehen, per GWT.create ein Objekt von PitchMenAdmin erzeugen
+	      pitchMenAdmin = GWT.create(PitchMenAdmin.class);
+	    }
+
+	    // Zurückgeben des Objekts
+	    return pitchMenAdmin;
+	  }
+
+	  /**
+	   * <p>
+	   * Anlegen und Auslesen dey applikationsweit eindeutigen ReportGenerator. Diese
+	   * Methode erstellt den ReportGenerator, wenn er noch nicht existiert. Ansonsten 
+	   * wird das bereits angelegte Objekt übergeben.
+	   * </p>
+	   * 
+	   * @return eindeutige Instanz des Typs <code>ReportGeneratorAsync</code>
+	   * @author Simon
+	   */
+	  public static ReportGeneratorAsync getReportGenerator() {
+	    if (reportGenerator == null) {
+	      // Instantiierung von ReportGenerator
+	      reportGenerator = GWT.create(ReportGenerator.class);
+
+	      final AsyncCallback<Void> initReportGeneratorCallback = new AsyncCallback<Void>() {
+	        @Override
+			public void onFailure(Throwable caught) {
+	          ClientsideSettings.getLogger().severe(
+	              "Der ReportGenerator konnte nicht initialisiert werden!");
+	        }
+
+	        @Override
+			public void onSuccess(Void result) {
+	          ClientsideSettings.getLogger().info(
+	              "Der ReportGenerator wurde initialisiert.");
+	        }
+	      };
+
+	      reportGenerator.init(initReportGeneratorCallback);
+	    }
+
+	    // Rückgabe des ReportGenerator
+	    return reportGenerator;
+	  }
+
+}
