a21abbbf269c198c77d9a4decdf85dbde1170e7f
SchelleLeon <ls104@hdm-stuttgart.de>
2017-05-31 19:46:21 +0200

    login Informationen der Klasse Person hinzugefügt

--- src/de/pitchMen/shared/bo/Person.java
+++ src/de/pitchMen/shared/bo/Person.java
@@ -16,8 +16,59 @@ public class Person extends OrganisationUnit {
 
 	private ArrayList<Project> projects = null;
 
+	private String eMail = ""; 
 
+	
+	private boolean loggedIn = false ; 
+	private String emailAdress = "";
+	private String nickname = "";
+	private String loginUrl = ""; 
+	private String logoutUrl = ""; 
+	
+	public boolean isLoggedIn(){
+		return loggedIn; 
+	}
 
+	public String getEmailAdress() {
+		return emailAdress;
+	}
+
+	public void setEmailAdress(String emailAdress) {
+		this.emailAdress = emailAdress;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public String getLoginUrl() {
+		return loginUrl;
+	}
+
+	public void setLoginUrl(String loginUrl) {
+		this.loginUrl = loginUrl;
+	}
+
+	public String getLogoutUrl() {
+		return logoutUrl;
+	}
+
+	public void setLogoutUrl(String logoutUrl) {
+		this.logoutUrl = logoutUrl;
+	}
+
+	public void setLoggedIn(boolean loggedIn) {
+		this.loggedIn = loggedIn;
+	}
+	
+	
+	
+	
+	
 	/**
 	 * @return firstName
 	 */
@@ -44,6 +95,14 @@ public class Person extends OrganisationUnit {
 	 */
 	public void setProjects(ArrayList<Project> projects) {
 		this.projects = projects;
+	}
+
+	public String getEmail() {
+		return eMail;
+	}
+
+	public void setEmail(String eMail) {
+		this.eMail = eMail;
 	}
 
 }
