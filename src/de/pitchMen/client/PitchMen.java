4c6ebbedf93f08ebf9520c48f5d938cbc9137bf3
SimonKienzler <simon.kienzler@gmx.de>
2017-06-01 18:18:17 +0200

    Klasse PitchMen reorganisiert und detaillierter kommentiert

--- src/de/pitchMen/client/PitchMen.java
+++ src/de/pitchMen/client/PitchMen.java
@@ -1,23 +1,14 @@
 package de.pitchMen.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Anchor;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.DockPanel;
-import com.google.gwt.user.client.ui.Grid;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
-import com.google.gwt.user.client.ui.Widget;
 
-import de.pitchMen.shared.PitchMenAdmin;
 import de.pitchMen.shared.PitchMenAdminAsync;
-import de.pitchMen.shared.ReportGeneratorAsync;
 import de.pitchMen.shared.bo.Person;
 
 /**
@@ -25,86 +16,130 @@ import de.pitchMen.shared.bo.Person;
  * beim Aufrufen der Applikation ausgeführt wird.
  */
 public class PitchMen implements EntryPoint {
+	
 	/**
 	 * Wird dem Nutzer ausgegeben, wenn der Server nicht erreichbar ist.
 	 */
 	private static final String SERVER_ERROR = "Beim Verbindungsaufbau mit dem Server ist ein Fehler aufgereten."
 			+ " Bitte überprüfen Sie Ihre Internetverbindung und versuchen Sie es erneut.";
-	/*
-	 * Instanzvariablen 
-	 */
-	private Person logInf = null;
-	private VerticalPanel logPanel = new VerticalPanel();
-	private Label logLabel = new Label(
-			"Bitte melden Sie sich bei Ihrem Google Account an, um die Webseite nutzen zu können");
-	private Anchor logLink = new Anchor("Anmelden");
+	
 	/**
 	 * Das Erzeugen eines <code>PitchMenAdmin</code>-Objekts ist nötig, um eine
 	 * zentrale Applikations-Verwaltung zu initialisieren, die die Aktivitäten
 	 * der Applikation steuert.
 	 */
-
-	// Zeile auskommentiert, da das Interface aktuell nicht lauffähig ist.
-	// Stand: 03.05.2017 19:00 Uhr - Simon
-	// private final PitchMenAdminAsync pitchMenAdmin =
-	// ClientsideSettings.getPitchMenAdmin();
+	//private final PitchMenAdminAsync pitchMenAdmin = ClientsideSettings.getPitchMenAdmin();
 
 	/**
 	 * Initialisierung eines <code>ReportGenerator</code>-Objekts ist nötig, um
 	 * einen zentralen ReportGenerator nutzen zu können.
 	 */
-
 	// Zeile auskommentiert, da das Interface aktuell nicht lauffähig ist.
 	// Stand: 03.05.2017 19:00 Uhr - Simon
-	// private final ReportGeneratorAsync reportGenerator =
-	// ClientsideSettings.getReportGenerator();
-
-	PitchMenAdminAsync pmVerwaltung = ClientsideSettings.getPitchMenAdmin();
+	// private final ReportGeneratorAsync reportGenerator = ClientsideSettings.getReportGenerator();
 
 	/**
 	 * Die Einstiegspunkt-Methode. Zunächst Anmeldung
 	 */
 	public void onModuleLoad() {
-
-	
-		pmVerwaltung.login(GWT.getHostPageBaseURL() + "PitchMen.html", new LoginCallback());
+		/*
+		 * TODO Die Login-Funktionalität kann erst dann umgesetzt
+		 * werden, wenn das Problem mit der AppEngine (angeschaltet 
+		 * kein Zugriff auf Datenbank, ausgeschaltet kein Zugriff 
+		 * auf UserService) gelöst wurde. Deshalb ist die folgende 
+		 * Zeile auskommentiert und die darunterliegenden extra
+		 * eingefügt. Damit wird die Anmeldung  zur Erstellung des User-
+		 * Objekts vermieden (gleichzeitig muss aber ein Dummy-User
+		 * in der ClientSide-Settings-Klasse erstellt werden, um
+		 * nutzerbezogene Aktionen zu ermöglichen).
+		 */
+		
+		// pitchMenAdmin.login(GWT.getHostPageBaseURL() + "PitchMen.html", new LoginCallback());
+		
+		Person dummy = new Person();					// später ersetzen
+		dummy.setFirstName("Armin");					// später ersetzen
+		dummy.setName("Austauschbar");					// später ersetzen
+		dummy.setId(1);									// später ersetzen
+		dummy.setLoggedIn(true);						// später ersetzen
+		dummy.setEmailAdress("arm.austausch@web.de");	// später ersetzen
+		dummy.setLogoutUrl("#");						// später ersetzen
+		ClientsideSettings.setCurrentUser(dummy);		// später ersetzen
+		
+		this.loadPitchMen();
 	}
 
+	/**
+	 * Die lokale Klasse LoginCallback wird instantiiert, wenn wir von der 
+	 * Applikationsschicht einen Callback bezüglich des Logins erwarten. 
+	 * Ihre Instantiierung erfolgt in der onModuleLoad()-Methode.
+	 * @author Leon
+	 *
+	 */
 	class LoginCallback implements AsyncCallback<Person> {
 
+		/*
+		 * Fehlerbehandlung, sollte ein Problem bei der 
+		 * Überprüfung des Logins auf Serverseite auftreten.
+		 *  
+		 * @see com.google.gwt.user.client.rpc.AsyncCallback#onFailure(java.lang.Throwable)
+		 */
 		public void onFailure(Throwable caught) {
 			ClientsideSettings.getLogger().severe("Login fehlgeschlagen");
 		}
 
-		public void onSuccess(Person rs) {
-
-			ClientsideSettings.setCurrentUser(rs);
-			// Überprüfen des Login Status
-			if (rs.isLoggedIn()) {
-				loadPitchMen();
+		// Der Aufruf kommt erfolgreich wieder zurück
+		public void onSuccess(Person person) {
+			/*
+			 * Sollte der Nutzer wider erwarten null sein, wird eine entsprechende
+			 * Fehlermeldung ausgegeben.
+			 */
+			if(person == null) {
+				ClientsideSettings.getLogger().severe("Zurückgegebenes User-Objekt ist null.");
 			}
-
-			else {
+			
+			// Die Person wird in den ClientsideSettings hinterlegt.
+			ClientsideSettings.setCurrentUser(person);
+			
+			// Ist der Nutzer bereits eingeloggt?
+			if (person.isLoggedIn()) {
+				// Dann lade die Applikation
+				loadPitchMen();
+			} else {	
+				// Ansonsten gebe einen Link zur Anmeldung aus
+				VerticalPanel logPanel = new VerticalPanel();
+				Label logLabel = new Label("Bitte melden Sie sich bei Ihrem Google Account an, um die Webseite nutzen zu können");
+				Anchor logLink = new Anchor("Anmelden");
 				
-				
-				logInf = rs;
-				logLink.setHref(logInf.getLoginUrl());
+				logLink.setHref(person.getLoginUrl());
 				logPanel.add(logLabel);
 				logPanel.add(logLink);
-				RootPanel.get("content").add(logPanel);
-			
+				logPanel.addStyleName("login-link-container");
+				
+				RootPanel.get("content").add(logPanel);			
 			}
 		}
-
 	}
+	
 	/*
 	 * Auslagerung des Startens in die Methode loadPitchMen(), da in der
 	 * onModuleLoad() Methode zunächst geprüft werden muss, ob der User
 	 * eingeloggt ist.
 	 * 
 	 */
-
 	private void loadPitchMen() {
+		
+		/*
+		 * Informationen zum angemeldeten User in der Topbar ausgeben.
+		 * Hierzu gehört auch der Logout-Link, über den der Nutzer
+		 * sich von der PitchMen-Applikation abmelden kann.
+		 */
+		RootPanel.get("top").add(new HTML("<p><span class='fa fa-user-circle-o'></span> &nbsp; " +
+				  ClientsideSettings.getCurrentUser().getFirstName() +
+				  " " +
+				  ClientsideSettings.getCurrentUser().getName() +
+				  "<a href='" +
+				  ClientsideSettings.getCurrentUser().getLogoutUrl() +
+				  "' title='Ausloggen'><span class='fa fa-sign-out'></span></a></p>"));
 
 		/*
 		 * Navigation-Objekt zur Darstellung der Navigationselemente
