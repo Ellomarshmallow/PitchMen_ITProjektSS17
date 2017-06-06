5ade42356d3c0d1e7bb7cd6f760cf4a289e8d4e9
SchelleLeon <ls104@hdm-stuttgart.de>
2017-06-06 17:06:15 +0200

    MarketplaceForm klasse erstellt

---
+++ src/de/pitchMen/client/elements/MarketplaceForm.java
@@ -0,0 +1,88 @@
+package de.pitchMen.client.elements;
+
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.HTML;
+
+import de.pitchMen.shared.bo.Marketplace;
+
+
+public class MarketplaceForm extends Formular {
+	
+	private int mpID ; 
+	
+
+	
+
+	public MarketplaceForm(int MarketplaceID){
+		super();  
+		
+		this.mpID = MarketplaceID; 
+		
+		super.getPitchMenAdmin().getMarketplaceByID(this.mpID, new MarketplaceFormCallback(this));
+		
+
+		
+		
+	}
+	
+	class MarketplaceFormCallback implements AsyncCallback<Marketplace>{
+		
+		private MarketplaceForm marketplaceform = null; 
+		
+		public MarketplaceFormCallback(MarketplaceForm marketplaceForm){
+			this.marketplaceform = marketplaceForm; 
+		}
+		
+		public void onFailure(Throwable caught){
+			
+			this.marketplaceform.add(new HTML("Fehler bei RPC Aufruf:" + caught.getMessage()));
+			
+		}
+		
+		public void onSuccess(Marketplace marketplace){
+			if (marketplace != null){
+				
+							this.marketplaceform.add(new HTML(marketplace.getTitle()));
+							this.marketplaceform.add(new HTML(marketplace.getDescription()));
+							
+							Button addMarketplaceBtn = new Button("+ Neuer Projektmarktplatz", new ClickHandler(){
+								
+																
+								public void onClick(ClickEvent event) {
+									
+								//bei Click wird die Methode run() aus der Klasse AddMarketplaceForm aufgerufen	
+								AddMarketplaceForm addMarketplace = new AddMarketplaceForm(); 
+								
+								addMarketplace.run(); 
+									
+							}}); 
+											
+																		
+							this.marketplaceform.add(addMarketplaceBtn); 
+							
+							//Neues Projekt Button:
+							
+							Button addProjectBtn = new Button("+ Neues Projekt hinzufügen", new ClickHandler(){
+								
+																
+								public void onClick(ClickEvent event) {
+									
+								AddProjectForm addProject = new AddProjectForm(); 
+								
+								addProject.run(mpID);
+									
+							}}); 
+											
+																		
+							this.marketplaceform.add(addProjectBtn); 
+							
+							
+				
+				
+			}
+		}
+	
+}}
