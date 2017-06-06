7239cb9a5bef723d4dda19d87de0b911ea8479fd
JuliusDigel <JuliusDigel@gmail.com>
2017-06-06 15:38:47 +0200

    Getter/Setter für jobPostingId erstellt

--- src/de/pitchMen/shared/report/AllApplicationsToOneJobPostingOfUser.java
+++ src/de/pitchMen/shared/report/AllApplicationsToOneJobPostingOfUser.java
@@ -2,4 +2,15 @@ package de.pitchMen.shared.report;
 
 public class AllApplicationsToOneJobPostingOfUser {
 
+	
+		private int jobPostingId;
+
+		public int getJobPostingId() {
+			return jobPostingId;
+		}
+
+		public void setJobPostingId(int jobPostingId) {
+			this.jobPostingId = jobPostingId;
+		}
+
 }
