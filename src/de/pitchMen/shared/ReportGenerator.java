455e17ff74b2572444778ef9a381616c957ab7b7
JuliusDigel <JuliusDigel@gmail.com>
2017-06-07 16:00:38 +0200

    AllParticipationsOfOneUser report Methode implementiert

--- src/de/pitchMen/shared/ReportGenerator.java
+++ src/de/pitchMen/shared/ReportGenerator.java
@@ -4,9 +4,11 @@ import de.pitchMen.shared.report.AllJobPostings;
 import de.pitchMen.shared.bo.JobPosting;
 import de.pitchMen.shared.bo.PartnerProfile;
 import de.pitchMen.shared.bo.Person;
+import de.pitchMen.shared.report.AllApplicationsOfOneUser;
 import de.pitchMen.shared.report.AllApplicationsOfUser;
 import de.pitchMen.shared.report.AllApplicationsToOneJobPostingOfUser;
 import de.pitchMen.shared.report.AllJobPostingsMatchingPartnerProfileOfUser;
+import de.pitchMen.shared.report.AllParticipationsOfOneUser;
 import de.pitchMen.shared.report.ApplicationsRelatedToJobPostingsOfUser;
 import de.pitchMen.shared.report.FanInJobPostingsOfUser;
 import de.pitchMen.shared.report.FanOutApplicationsOfUser;
@@ -44,6 +46,14 @@ public interface ReportGenerator extends RemoteService {
 	* 
 	*/
 	public abstract AllApplicationsOfUser showAllApplicationsOfUser(Person p) throws IllegalArgumentException;
+	/**
+	 * 
+	 */
+	public abstract AllApplicationsOfOneUser showAllApplicationsOfOneUser(int id) throws IllegalArgumentException;
+	/**
+	 * 
+	 */
+	public abstract AllParticipationsOfOneUser showAllParticipationsOfOneUser (int id)  throws IllegalArgumentException;
 	/**
 	 * 
 	 */
