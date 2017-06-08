a614036cff244d60adf71bc618b25fe2be414f31
LeenenLars <ll038@hdm-stuttgart.de>
2017-06-08 10:55:02 +0200

    Methode findPartnerProfileByjobPostingId geschrieben und Datenbankabfrage zuvor getestet.

--- src/de/pitchMen/server/db/PartnerProfileMapper.java
+++ src/de/pitchMen/server/db/PartnerProfileMapper.java
@@ -213,5 +213,55 @@ public class PartnerProfileMapper {
 		}
 		return result;
 	}
+	
+	
+	/**
+	 * Findet ein PartnerProfile-Tupel anhand der übergebenen jobPostingId in der
+	 * Datenbank.Anschließend wird es als Java-Objekt in der Variablen partnerProfile 
+	 * vom typ PartnerProfile gespeichert. Methode zum Suchen von Partnerprofilen 
+	 * bestehende Beziehungen/Datensätze zu löschen
+	 * 
+	 * @param jobPostingId
+	 * @return partnerProfil
+	 */
+	public PartnerProfile findPartnerProfileByjobPostingId(int jobPostingId) {
+		
+		Connection con = DBConnection.connection();
+
+		try {
+			// leeres SQL-Statement (JDBC) anlegen
+			Statement stmt = con.createStatement();
+
+			// Statement ausfüllen und als Query an die Datenbank senden
+			ResultSet rs = stmt.executeQuery(
+					"SELECT * FROM partnerProfile "
+					+ "WHERE jobPosting_id = " + jobPostingId);
+
+			/**
+			 * Zu einer JobPostingId exisitiert nur max ein Datenbank-Tupel,
+			 * somit kann auch nur einer zurückgegeben werden. Es wird mit einer
+			 * IF-Abfrage geprüft, ob es für den angefragten Primärschlüssel ein
+			 * DB-Tupel gibt.
+			 */
+
+			if (rs.next()) {
+				// Ergebnis-Tupel in Objekt umwandeln
+				PartnerProfile partnerProfile = new PartnerProfile();
+				partnerProfile.setId(rs.getInt("id"));
+				partnerProfile.setDateCreated(rs.getDate("dateCreated"));
+				partnerProfile.setDateChanged(rs.getDate("dateChanged"));
+				partnerProfile.setCompanyId(rs.getInt("company_id"));
+				partnerProfile.setTeamId(rs.getInt("team_id"));
+				partnerProfile.setPersonId(rs.getInt("person_id"));
+				partnerProfile.setJobPostingId(rs.getInt("jobPosting_id"));
+
+				return partnerProfile;
+			}
+
+		} catch (SQLException e2) {
+			e2.printStackTrace();
+		}
+		return null;
+	}
 
 }
