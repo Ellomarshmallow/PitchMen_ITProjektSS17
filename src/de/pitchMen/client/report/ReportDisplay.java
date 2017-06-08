e1995afcc4e1fe6d6cc407985abb688a40db146f
SimonKienzler <simon.kienzler@gmx.de>
2017-06-08 18:38:00 +0200

    Klasse ReportDisplay angelegt

---
+++ src/de/pitchMen/client/report/ReportDisplay.java
@@ -0,0 +1,21 @@
+package de.pitchMen.client.report;
+
+import com.google.gwt.core.client.EntryPoint;
+
+/**
+ * Diese Klasse ist der Einsteigsprunkt für die Report-Funktionen
+ * der PitchMen-Applikation.
+ * 
+ * @author Simon
+ *
+ */
+
+public class ReportDisplay implements EntryPoint {
+
+	@Override
+	public void onModuleLoad() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
