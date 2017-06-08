be8615c0177e4cdde14d92d160483f9cbe7abeda
LeenenLars <ll038@hdm-stuttgart.de>
2017-06-08 12:23:09 +0200

    Methode findPartnerProfileByTeamId geschrieben. Anpassungen folgen

--- src/de/pitchMen/server/db/PartnerProfileMapper.java
+++ src/de/pitchMen/server/db/PartnerProfileMapper.java
@@ -311,6 +311,52 @@ public class PartnerProfileMapper {
 		}
 		return null;
 	}
+	
+	/**
+	 * Methode zum Suchen von Partnerprofilen 
+	 * bestehende Beziehungen/Datensätze zu löschen
+	 * 
+	 * @param companyId
+	 * @return partnerProfil
+	 */
+	public PartnerProfile findPartnerProfileByTeamId(int teamId) {
+		
+		Connection con = DBConnection.connection();
+
+		try {
+			
+			Statement stmt = con.createStatement();
+
+			// Statement ausfüllen und als Query an die Datenbank senden
+			ResultSet rs = stmt.executeQuery(
+					"SELECT * FROM partnerProfile "
+					+ "WHERE company_id = " + teamId);
+
+			/**
+			 * Zu einer teamId exisitiert nur max ein Datenbank-Tupel,
+			 * somit kann auch nur einer zurückgegeben werden. Es wird mit einer
+			 * 
+			 */
+
+			if (rs.next()) {
+				// Ergebnis-Tupel in PartnerProfile-Objekt gespeichert.
+				PartnerProfile partnerProfile = new PartnerProfile();
+				partnerProfile.setId(rs.getInt("id"));
+				partnerProfile.setDateCreated(rs.getDate("dateCreated"));
+				partnerProfile.setDateChanged(rs.getDate("dateChanged"));
+				partnerProfile.setCompanyId(rs.getInt("company_id"));
+				partnerProfile.setTeamId(rs.getInt("team_id"));
+				partnerProfile.setPersonId(rs.getInt("person_id"));
+				partnerProfile.setJobPostingId(rs.getInt("jobPosting_id"));
+				//Zurückgegebendes PartnerProfil-Objekt beinhaltet das PartnerProfil zu der companyID
+				return partnerProfile;
+			}
+
+		} catch (SQLException e2) {
+			e2.printStackTrace();
+		}
+		return null;
+	}
 
 
 }
