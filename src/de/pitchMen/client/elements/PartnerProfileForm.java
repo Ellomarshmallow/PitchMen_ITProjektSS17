ba22f0d0605acac4bed7e00018ca8f8e91ea4b97
SimonKienzler <simon.kienzler@gmx.de>
2017-06-10 14:30:28 +0200

    Klasse PartnerProfileForm angelegt

---
+++ src/de/pitchMen/client/elements/PartnerProfileForm.java
@@ -0,0 +1,59 @@
+package de.pitchMen.client.elements;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+import de.pitchMen.client.ClientsideSettings;
+import de.pitchMen.shared.PitchMenAdminAsync;
+import de.pitchMen.shared.bo.PartnerProfile;
+
+/**
+ * Die Klasse <code>PartnerProfileForm</code> stellt
+ * Funktionalitäten für die Erstellung eines Formulars
+ * zur Verfügung, mit dessen Hilfe Partnerprofile 
+ * bearbeitet werden können. Sie erbt von der Klasse
+ * {@link Formular}.
+ * 
+ * @author Simon
+ */
+public class PartnerProfileForm extends Formular {
+	
+	/**
+	 * Der angemeldete Nutzer muss dem Formular bekannt sein,
+	 * um ihm das richtige PartnerProfile zur Bearbeitung
+	 * ausgeben zu können.
+	 */
+	int currentUserId = 0;
+	
+	/**
+	 * Auch in dieser Klasse werden die Funktionalitäten der
+	 * {@link de.pitchMen.server.PitchMenAdminImpl} benötigt.
+	 */
+	PitchMenAdminAsync pitchMenAdmin = ClientsideSettings.getPitchMenAdmin();
+	
+	public PartnerProfileForm() {
+		// Abfrage der id des aktuell angemeldeten Nutzers
+		this.currentUserId = ClientsideSettings.getCurrentUser().getId();
+		this.pitchMenAdmin.getPartnerProfileByID(currentUserId, new PartnerProfileCallback());
+	}
+	
+	/**
+	 * Die genestete Klasse <code>PartnerProfileCallback</code>
+	 * behandelt die zurückkehrende Server-Abfrage eines
+	 * Partnerprofils.
+	 */
+	private class PartnerProfileCallback implements AsyncCallback<PartnerProfile> {
+
+		@Override
+		public void onFailure(Throwable caught) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void onSuccess(PartnerProfile result) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+}
