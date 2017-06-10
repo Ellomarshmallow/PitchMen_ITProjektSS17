3f022c0440c107945058e0baa76a3bfe8a48e901
SchelleLeon <ls104@hdm-stuttgart.de>
2017-06-10 17:15:03 +0200

    JobPostingForm, ClickHandler und Bewerben-Button hinzugefügt

--- src/de/pitchMen/client/elements/JobPostingForm.java
+++ src/de/pitchMen/client/elements/JobPostingForm.java
@@ -1,10 +1,15 @@
 package de.pitchMen.client.elements;
 
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
+import de.pitchMen.client.elements.ProjectForm.DeleteProjectCallback;
 import de.pitchMen.client.elements.ProjectForm.addJobPostingClickHandler;
 import de.pitchMen.client.elements.ProjectForm.addProjectClickHandler;
 import de.pitchMen.client.elements.ProjectForm.deleteProjectClickHandler;
@@ -58,13 +63,18 @@ public class JobPostingForm extends Formular{
 				HorizontalPanel buttonsPanel = new HorizontalPanel();
 				this.add(buttonsPanel);
 				
+				/* ---------- Bewerben-Button, ClickHandler hinzufügen und 
+	 						* dem HorizontalPanel hinzufügen */
+				Button applicateButton = new Button ("Hier Bewerben"); 
+				applicateButton.addClickHandler(new applicateClickHandler());
+				buttonsPanel.add(applicateButton);
+				
 				/*
 				 * Wenn die aktuelle UserId gleich der PersonId ist, dann hat dieser die
 				 * Buttons Ausschreibung hinzufügen, Löschen und Bearbeiten zur verfügung.
 				 * Vgl. hasPermission() in Formular.java
 				 */
 				if (hasPermission(this.selectedProject)) {
-
 					/* ---------- Neue Ausschreibung-Button, ClickHandler hinzufügen und 
 					 			* dem HorizontalPanel hinzufügen */
 					Button addJobPostingBtn = new Button("+ Neue Ausschreibung hinzufügen");
@@ -93,11 +103,94 @@ public class JobPostingForm extends Formular{
 
 		// ---------- ClickHandler
 	
-	
-	
-	
+	// ---------- addJobPostingClickHandler
+		private class addJobPostingClickHandler implements ClickHandler {
 
-	public void setSelectedJobPosting(JobPosting jobPosting) {
-		this.selectedJobPosting = jobPosting;
-	}
+			public void onClick(ClickEvent event) {
+
+				AddJobPostingForm addJobPosting = new AddJobPostingForm();
+
+			}
+		}
+	
+		// ---------- deleteJobPostingClickHandler
+		private class deleteJobPostingClickHandler implements ClickHandler {
+
+			public void onClick(ClickEvent event) {
+
+				// bei Click wird die delete() Methode aufgerufen
+
+				if (Window.confirm("Sind Sie sich sicher, dass Sie das löschen wollen?")) {
+					delete();
+				}
+
+			}
+		}
+
+		// ---------- updateJobPostingClickHandler
+		private class updateJobPostingClickHandler implements ClickHandler {
+			public void onClick(ClickEvent event) {
+
+				// bei Click wird die update() Methode aufgerufen
+				JobPostingForm updateJobPosting = new JobPostingForm(selectedProject,pitchMenTreeViewModel,false);
+
+			}
+		} 
+
+		public void delete() {
+			
+			super.getPitchMenAdmin().deleteJobPosting(selectedJobPosting, new DeleteJobPostingCallback(selectedJobPosting));
+
+
+		}
+
+		class DeleteJobPostingCallback implements AsyncCallback<Void> {
+
+			JobPosting j = null;
+
+			public DeleteJobPostingCallback(JobPosting j) {
+				this.j = j;
+			}
+
+			public void onFailure(Throwable caught) {
+				Window.alert("Das Löschen der Ausschreibung ist fehlgeschlagen!");
+
+			}
+
+			public void onSuccess(Void result) {
+				if (j != null) {
+					setSelectedJobPosting(null);
+				
+					pitchMenTreeViewModel.deleteJobPosting(selectedJobPosting, selectedProject);
+				}
+			}
+		}
+		
+		// ----------pitchMenTreeViewModelsetter
+		public void setPitchMenTreeViewModel(PitchMenTreeViewModel pitchMenTreeViewModel) {
+			this.pitchMenTreeViewModel = pitchMenTreeViewModel;
+		}
+
+		
+		
+		// ---------- selectedProject setter
+				public void setSelectedJobPosting(JobPosting j) {
+					if (j != null) {
+						this.selectedJobPosting = j;
+						titleBox.setText(selectedJobPosting.getTitle());
+						descBox.setText(selectedJobPosting.getText()); 						
+						statusBox.setText(selectedJobPosting.getStatus());
+						deadlineBox.setText(selectedJobPosting.getStatus());
+						idLabel.setText(Integer.toString(selectedProject.getId()));
+					} 
+					else {
+						idLabel.setText("");
+						titleBox.setText("");
+						descBox.setText("");
+						statusBox.setText("");
+						deadlineBox.setText("");
+						
+					}
+				}
+	
 }
