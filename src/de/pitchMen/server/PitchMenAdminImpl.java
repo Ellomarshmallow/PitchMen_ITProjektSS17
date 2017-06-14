b9497b11b8c2ea32f2a2551990bccaef511e52be
SimonKienzler <simon.kienzler@gmx.de>
2017-06-14 15:03:27 +0200

    Logger-Nachrichten eingebaut, severe gesetzt

--- src/de/pitchMen/server/PitchMenAdminImpl.java
+++ src/de/pitchMen/server/PitchMenAdminImpl.java
@@ -666,6 +666,8 @@ public class PitchMenAdminImpl extends RemoteServiceServlet implements PitchMenA
 
 	public Person login(String requestUri) {
 
+		ClientsideSettings.getLogger().severe("login()-Methode wurde aufgerufen.");
+		
 		UserService userService = UserServiceFactory.getUserService();
 		User user = userService.getCurrentUser();
 		
@@ -680,6 +682,8 @@ public class PitchMenAdminImpl extends RemoteServiceServlet implements PitchMenA
 		if (user != null) {
 			// Der Nutzer hat die erste Hürde genommen und sich angemeldet
 			
+			ClientsideSettings.getLogger().severe("User-Objekt ist nicht null.");
+			
 			/*
 			 * Wir fragen zunächst bei der Datenbank an, ob der Nutzer,
 			 * der sich gerade angemeldet hat, bereits vorhanden ist.
@@ -693,7 +697,7 @@ public class PitchMenAdminImpl extends RemoteServiceServlet implements PitchMenA
 			 */
 			if(existingPerson != null){
 				// Der Nutzer ist dem System bereits bekannt.
-				ClientsideSettings.getLogger().info("User mit der E-Mai-Adresse [" + user.getEmail()
+				ClientsideSettings.getLogger().severe("User mit der E-Mai-Adresse [" + user.getEmail()
 						+ "]  existiert.");
 
 				/*
@@ -721,6 +725,8 @@ public class PitchMenAdminImpl extends RemoteServiceServlet implements PitchMenA
 		} 
 		else {
 			// Hier landen wir wenn der Nutzer nicht angemeldet ist
+			
+			ClientsideSettings.getLogger().severe("User-Objekt ist null.");
 			
 			/*
 			 * Mit dem setzen dieses Wertes auf false teilen wir der GUI
