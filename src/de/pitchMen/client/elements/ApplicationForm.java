e7d12afdb8529bd91bf87ddc69ff712a60285ed1
SimonKienzler <simon.kienzler@gmx.de>
2017-06-14 10:59:51 +0200

    Klasse ApplicationForm angelegt

---
+++ src/de/pitchMen/client/elements/ApplicationForm.java
@@ -0,0 +1,13 @@
+package de.pitchMen.client.elements;
+
+/**
+ * Klasse, deren Objekte ein Formular
+ * zur Bewerbung auf eine Ausschreibung 
+ * dienen.
+ * 
+ * @author Simon
+ */
+
+public class ApplicationForm extends Formular {
+
+}
