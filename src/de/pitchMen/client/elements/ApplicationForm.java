05028e498c9fbb049d5257cc1b15db0b9c31e4a8
SimonKienzler <simon.kienzler@gmx.de>
2017-06-14 11:06:20 +0200

    ApplicationForm: Attribut referredJobPosting und Konstruktor ergänzt

--- src/de/pitchMen/client/elements/ApplicationForm.java
+++ src/de/pitchMen/client/elements/ApplicationForm.java
@@ -1,5 +1,7 @@
 package de.pitchMen.client.elements;
 
+import de.pitchMen.shared.bo.JobPosting;
+
 /**
  * Klasse, deren Objekte ein Formular
  * zur Bewerbung auf eine Ausschreibung 
@@ -9,5 +11,23 @@ package de.pitchMen.client.elements;
  */
 
 public class ApplicationForm extends Formular {
-
+	
+	/**
+	 * Um die Zugehörigkeit der mit dem Formular
+	 * zu erstellenden Bewerbung zu einem Projekt
+	 * feststellen zu können, benötigen Objekte der
+	 * Klasse {@link ApplicationForm} ein Attribut
+	 * <code>referredJobPosting</code> vom Typ
+	 * {@link JobPosting}. 
+	 */
+	private JobPosting referredJobPosting = null;
+	
+	/**
+	 * Beim Anlegen eines neuen <code>ApplicationForm</code>
+	 * Objekts wird das JobPosting-Objekt übergeben, auf das
+	 * sich die anzulegende Bewerbung beziehen soll.
+	 */
+	public ApplicationForm(JobPosting jobPosting) {
+		this.referredJobPosting = jobPosting;
+	}
 }
