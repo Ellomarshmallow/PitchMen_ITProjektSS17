2d6c60d59f9e66a337922d47cc6a74331f2868bd
JuliusDigel <JuliusDigel@gmail.com>
2017-06-17 15:09:27 +0200

    get Rows in FanInAndOutReport hinzugefügt

--- src/de/pitchMen/shared/report/FanInAndOutReport.java
+++ src/de/pitchMen/shared/report/FanInAndOutReport.java
@@ -1,10 +1,17 @@
 package de.pitchMen.shared.report;
 
+import java.util.ArrayList;
+
 public class FanInAndOutReport extends CompositeReport {
 	/**
      * 
      */
     private static final long serialVersionUID = 1L;
+
+	public ArrayList<Row> getRows() {
+		// TODO Auto-generated method stub
+		return null;
+	}
     
     
     
