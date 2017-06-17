8516e853a64fc741a003367e60a132bf10a74eb9
JuliusDigel <JuliusDigel@gmail.com>
2017-06-17 14:15:22 +0200

    getAllJobPostings/MatchingPPofUser Methoden hinzugefügt

--- src/de/pitchMen/client/report/ReportNavigation.java
+++ src/de/pitchMen/client/report/ReportNavigation.java
@@ -9,7 +9,11 @@ import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
+import de.pitchMen.client.ClientsideSettings;
+import de.pitchMen.shared.bo.PartnerProfile;
+import de.pitchMen.shared.bo.Person;
 import de.pitchMen.shared.report.AllJobPostings;
+import de.pitchMen.shared.report.AllJobPostingsMatchingPartnerProfileOfUser;
 import de.pitchMen.shared.report.HTMLReportWriter;
 
 /**
@@ -24,7 +28,7 @@ import de.pitchMen.shared.report.HTMLReportWriter;
 
 public class ReportNavigation extends VerticalPanel {
 	
-	
+	private HTML reportContent = null;
 
 	public void onLoad() {
 		/*
@@ -71,6 +75,7 @@ public class ReportNavigation extends VerticalPanel {
 	 */
 	private class ReportClickHandler implements ClickHandler {
 		
+		
 		/**
 		 * Die Report-Nummer wird als Instanzvariable deklariert
 		 * und im Konstruktor gesetzt. Somit ist über den 
@@ -93,9 +98,9 @@ public class ReportNavigation extends VerticalPanel {
 		public void onClick(ClickEvent event) {
 			HTML reportContent = null;
 			switch (reportNo) {
-				case 1: reportContent = this.getAllApplications();
+				case 1: reportContent = this.getAllJobPostings();
 						break;
-				case 2: reportContent = this.getAllApplicationsMatchingPartnerProfileOfUser();
+				case 2: reportContent = this.getAllJobPostingsMatchingPartnerProfileOfUser();
 						break;
 				case 3: reportContent = this.getAllApplicationOnJobPostingsOfUser();
 						break;
@@ -133,50 +138,59 @@ public class ReportNavigation extends VerticalPanel {
 			// TODO Auto-generated method stub
 			
 		}
+		
+		private HTML getAllJobPostingsMatchingPartnerProfileOfUser() {
+			final HTMLReportWriter writer = new HTMLReportWriter();
+			ClientsideSettings.getPitchMenAdmin().getPartnerProfileByPersonId(ClientsideSettings.getCurrentUser().getId(), new AsyncCallback<PartnerProfile>() {
 
-		private HTML getAllApplicationsMatchingPartnerProfileOfUser() {
-			return null;
-			// TODO Auto-generated method stub
-			
-		}
+				@Override
+				public void onFailure(Throwable caught) {
+					// TODO Auto-generated method stub
+					
+				}
 
-		private HTML getAllApplications() {
-			
-//			class ShowAllJobPostings extends VerticalPanel {
-//				
-//				public void onLoad() {
-//					super.onLoad();
-//					this.add(new HTML("<h2>Alle Ausschreibungen</h2>"));
-//					
-//					ReportGeneratorAsync reportGenerator = ClientsideSettings.getReportGenerator();
-//					
-//					reportGenerator.showAllJobPostings(new AllJobPostingsCallback(this));
-//				}
-//				
-//				
-//			}
-			return null;
-		}
-		class AllJobPostingsCallback implements AsyncCallback<AllJobPostings> {
-			
-			private ClickHandler parent = null;
-			
-			public AllJobPostingsCallback(ClickHandler parent) {
-				this.parent = parent;
-			}
+				@Override
+				public void onSuccess(PartnerProfile result) {
+					ClientsideSettings.getReportGenerator().showAllJobPostingsMatchingPartnerProfileOfUser(result, new AsyncCallback<AllJobPostingsMatchingPartnerProfileOfUser>() {
 
-			@Override
-			public void onFailure(Throwable caught) {
-				((AbsolutePanel) parent).add(new HTML("<p class='error'>Konnte Ausschreibungen nicht laden.</p>"));
+						@Override
+						public void onFailure(Throwable caught) {
+							// TODO Auto-generated method stub
+							
+						}
+
+						@Override
+						public void onSuccess(AllJobPostingsMatchingPartnerProfileOfUser result) {
+							writer.process(result);
+							reportContent = new HTML(writer.getReportText());
+						}
+						
+					});
+				}
 				
-			}
+			});
+			return reportContent;
+	}
 
-			@Override
-			public void onSuccess(AllJobPostings report) {
-				HTMLReportWriter writer = new HTMLReportWriter();
-				writer.process(report);
-		        ((AbsolutePanel) parent).add(new HTML(writer.getReportText()));
-			}
+		private HTML getAllJobPostings() {
+			final HTMLReportWriter writer = new HTMLReportWriter();
+			ClientsideSettings.getReportGenerator().showAllJobPostings(new AsyncCallback<AllJobPostings>() {
+					
+						@Override
+						public void onSuccess(AllJobPostings result) {
+						writer.process(result);
+						reportContent = new HTML(writer.getReportText());
+						
+						}
+
+						@Override
+						public void onFailure(Throwable caught) {
+						// TODO Auto-generated method stub
+						
+						}
+			});
+			return reportContent;
 		}
 	}
+	
 }
