242cc438ae0ab464ff6fe9f7d98732781b98ab28
LeenenLars <ll038@hdm-stuttgart.de>
2017-06-22 09:07:47 +0200

    Attribute aus der SubKlasse in die Überklasse übertragen,damit das mehrmalige Aufrufen verhindert wird.

--- src/de/pitchMen/shared/report/AllJobPostings.java
+++ src/de/pitchMen/shared/report/AllJobPostings.java
@@ -15,15 +15,5 @@ public class AllJobPostings extends SimpleReport {
     private static final long serialVersionUID = 1L;
     
        
-    private int jobPostingId;
-
-
-	public int getJobPostingId() {
-		return jobPostingId;
-	}
-
-
-	public void setJobPostingId(int jobPostingId) {
-		this.jobPostingId = jobPostingId;
-	}
+ 
 }
