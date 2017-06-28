862b87069027e3a8e113dfa5b5a84167ffb528cf
LeenenLars <ll038@hdm-stuttgart.de>
2017-06-28 09:08:49 +0200

    personId sowie die dazugehörigen getter und Setter gesetzt

--- src/de/pitchMen/shared/report/AllApplicationsOfUser.java
+++ src/de/pitchMen/shared/report/AllApplicationsOfUser.java
@@ -1,33 +1,37 @@
 package de.pitchMen.shared.report;
 
 /**
- * Report zur Abfrage von allen Bewerbungen auf Ausschreibungen des Benutzers.  
+ * Report zur Abfrage von allen Bewerbungen auf Ausschreibungen des Benutzers.
  * Subklasse von SimpleReport. Enthält keine eigenen Attribute und Methoden.
- * Dient der Objektorientierten-Programmierung. 
+ * Dient der Objektorientierten-Programmierung.
  * 
  * @author
  */
-public class AllApplicationsOfUser extends SimpleReport{
+public class AllApplicationsOfUser extends SimpleReport {
 
-	  /**
-     * 
-     */
-    private static final long serialVersionUID = 1L;
+	/**
+	* 
+	*/
+	private static final long serialVersionUID = 1L;
 
-//  private int applicationId;
-    private int personId;
-    
-    
+	// private int applicationId;
+
+	/**
+	 * 
+	 */
+	private int personId;
+
 	public int getPersonId() {
 		return personId;
 	}
+
 	public void setPersonId(int personId) {
 		this.personId = personId;
 	}
-//	public int getApplicationId() {
-//		return applicationId;
-//	}
-//	public void setApplicationId(int applicationId) {
-//		this.applicationId = applicationId;
-//	}
+	// public int getApplicationId() {
+	// return applicationId;
+	// }
+	// public void setApplicationId(int applicationId) {
+	// this.applicationId = applicationId;
+	// }
 }
