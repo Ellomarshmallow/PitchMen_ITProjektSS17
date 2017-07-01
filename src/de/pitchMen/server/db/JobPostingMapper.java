56e0712484781550b541eeba3c8e2013136b956c
LeenenLars <ll038@hdm-stuttgart.de>
2017-07-01 17:41:27 +0200

    SQL Query für AllJobPostings angepasst, damit Sortierreihenfolge Sinn ergibt

--- src/de/pitchMen/server/db/JobPostingMapper.java
+++ src/de/pitchMen/server/db/JobPostingMapper.java
@@ -210,7 +210,7 @@ public class JobPostingMapper {
 			/**
 			 * SQL-Anweisung zum Finden aller Datensatzes in der Datenbank, sortiert nach der Id.
 			 */
-			ResultSet rs = stmt.executeQuery("SELECT * FROM jobPosting ORDER BY status");
+			ResultSet rs = stmt.executeQuery("SELECT * FROM jobPosting ORDER BY status, title");
 			/**
 			 * Da es sein kann, dass mehr als nur ein Datenbank-Tupel in der
 			 * Tabelle jobPosting vorhanden ist, muss das Abfragen des ResultSet so
